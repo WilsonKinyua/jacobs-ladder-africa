e5e4f0a6c39966797d6d055d43905ee1a6367c2d
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-03-15 16:51:30 +0300

    pillars carousel implemented

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  // pillars carousel
+  $('.pillars-carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+        breakpoint: 1500,
+        settings: {
+            slidesToShow: 2
+        }
+    }, {
+        breakpoint: 650,
+        settings: {
+            slidesToShow:1
+        }
+    }]
+});
+});
