fae55f663f76d8a2056cc765c190fe7d445c2924
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-06-23 20:13:52 +0300

    admin set up implemented

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -12,9 +12,9 @@ class UsersTableSeeder extends Seeder
         $users = [
             [
                 'id'                 => 1,
-                'name'               => 'Admin',
-                'email'              => 'admin@admin.com',
-                'password'           => bcrypt('password'),
+                'name'               => 'Super Admin',
+                'email'              => 'admin@jacobsladder.africa',
+                'password'           => bcrypt('5l9x+c7s'),
                 'remember_token'     => null,
                 'approved'           => 1,
                 'verified'           => 1,
